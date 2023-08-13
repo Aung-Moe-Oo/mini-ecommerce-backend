0fbb4af87fa5949d410829d00eb0cb94439330eb
Sai Aung Moe Oo <saiaungmoeoo.lk@gmail.com>
2023-08-13 15:05:57 +0630

    fix prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { InvoiceModule } from './invoice/invoice.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ProductModule, AuthModule, UserModule, InvoiceModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
